1dab235d045f0e48b6c0200c531666cb547206f3
AkosuaTakyiwaa <nhyiradenkyi@gmail.com>
2023-08-24 14:05:55 -0700

    update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,7 +9,7 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t s = 0;
+	size_t z = 0;
 
 	while (h)
 	{
@@ -18,8 +18,8 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		s++;
+		z++;
 	}
 
-	return (s);
+	return (z);
 }
